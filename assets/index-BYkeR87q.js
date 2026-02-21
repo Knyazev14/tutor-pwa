d3b2fd749ca0d64b70c69ce9af4c09565ffa520d
Artem <razzylom@mail.ru>
2026-02-21 21:39:26 +0300

    Updates

--- assets/index-BYkeR87q.js
+++
@@ -1,2 +0,0 @@
-import{e as r}from"./index-DrmWgVJa.js";var o=r();export{o as r};
-//# sourceMappingURL=index-BYkeR87q.js.map
---
+++ assets/index-Bwj3LqNz.js
@@ -0,0 +1,2 @@
+import{e as r}from"./index-CxWwzzE3.js";var o=r();export{o as r};
+//# sourceMappingURL=index-Bwj3LqNz.js.map
