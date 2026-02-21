863d35ee51cb5fe546aec8497a71402c9f03a87a
Artem <razzylom@mail.ru>
2026-02-21 20:37:15 +0300

    Updates

---
+++ assets/index-CabAxUVw.js
@@ -0,0 +1,2 @@
+import{e as r}from"./index-A6qFd9pG.js";var o=r();export{o as r};
+//# sourceMappingURL=index-CabAxUVw.js.map
---
+++ assets/lesson.api-CG9oOzTK.js
@@ -0,0 +1,2 @@
+import{i as t}from"./index-A6qFd9pG.js";const n={getAll(){return t.get("api/v1/lesson/get")},getById(e){return t.get(`api/v1/lesson/get/${e}`)},create(e){return t.post("api/v1/lesson/create",e)},update(e,s){return t.put(`api/v1/lesson/update/${e}`,s)},delete(e){return t.delete(`api/v1/lesson/delete/${e}`)}};export{n as L};
+//# sourceMappingURL=lesson.api-CG9oOzTK.js.map
